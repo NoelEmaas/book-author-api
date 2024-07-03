7ee500792e3594b9c76e969c071b982e70d5eb0f
NoelEmaas <nemaas.dev@gmail.com>
2024-07-03 10:59:20 +0800

    feat: injected book service and added routes

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  getBooks(
+    @Query('genre') genre: string,
+    @Query('authorId') authorId: number,
+  ) {
+    return this.booksService.getBooks({ genre, authorId });
+  }
+
+  @Get(':id')
+  getBook(@Param('id') id: string) {
+    return this.booksService.getBook(+id);
+  }
+
+  @Post()
+  createBook(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.createBook(createBookDto);
+  }
+
+  @Put(':id')
+  updateBook(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+    return this.booksService.updateBook(+id, updateBookDto);
+  }
+
+  @Delete(':id')
+  deleteBook(@Param('id') id: string) {
+    return this.booksService.deleteBook(+id);
+  }
+
+}
