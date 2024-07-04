7adbcb3dadce1828dea68f45032714c1fc53f1d3
NoelEmaas <nemaas.dev@gmail.com>
2024-07-04 22:50:52 +0800

    feat: added global validation pipe for handling validation errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
+import { ValidationPipe, BadRequestException, ValidationError } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new BadRequestException(
+          validationErrors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints).join(', '),
+          })),
+        );
+      },
+    }),
+  );
   await app.listen(3000);
 }
 
