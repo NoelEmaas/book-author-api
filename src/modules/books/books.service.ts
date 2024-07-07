06467ef6b7796c5b44aec945a6fd4b3b77e4bc7e
NoelEmaas <nemaas.dev@gmail.com>
2024-07-07 19:19:17 +0800

    feat: added definition for adding and removing authors abstract methods

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,12 +1,16 @@
+import { AuthorsJsonDBService } from '../authors/authors-json-db.service';
 import { BooksJsonDBService } from './books-json-db.service';
 import { BooksServiceBase } from './books-abstract.service';
 import { BookType } from 'src/types/book.types';
 import { CreateBookDto, UpdateBookDto } from './dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class BooksService implements BooksServiceBase {
-  constructor(private readonly booksJsonDBService: BooksJsonDBService) {}
+  constructor(
+    private readonly booksJsonDBService: BooksJsonDBService,
+    private readonly authorsJsonDBService: AuthorsJsonDBService
+  ) {}
 
   getBooks(bookFilter: Partial<BookType> & { search?: string }): BookType[] {
     return this.booksJsonDBService.getAll(bookFilter);
@@ -45,5 +49,44 @@ export class BooksService implements BooksServiceBase {
 
     this.booksJsonDBService.delete(id);
     return bookToRemove;
+  }
+
+  addAuthorToBook(bookId: string, authorId: string): BookType {
+    const book = this.booksJsonDBService.get(bookId);
+    const author = this.authorsJsonDBService.get(authorId);
+
+    if (!author) {
+      throw new NotFoundException('Author you are trying to add to book does not exist');
+    }
+
+    if (!book) {
+      throw new NotFoundException('Book you are trying to add author to does not exist');
+    }
+
+    if (book.authorIds.includes(authorId)) {
+      throw new ConflictException('Author you are trying to add to book already exists');
+    }
+
+    book.authorIds.push(authorId);
+    return this.booksJsonDBService.update(bookId, book);
+  }
+
+  removeAuthorFromBook(bookId: string, authorId: string): BookType {
+    const book = this.booksJsonDBService.get(bookId);
+
+    if (!book) {
+      throw new NotFoundException('Book you are trying to remove author from does not exist');
+    }
+
+    if (!book.authorIds.includes(authorId)) {
+      throw new NotFoundException('Author you are trying to remove from book does not exist');
+    }
+
+    if (book.authorIds.length === 1) {
+      throw new BadRequestException('Book must have at least one author');
+    }
+
+    book.authorIds = book.authorIds.filter((id) => id !== authorId);
+    return this.booksJsonDBService.update(bookId, book);
   }
 }
