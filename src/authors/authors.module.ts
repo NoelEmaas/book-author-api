6f17b25bc4501841f5b5044f958d95920ba21457
NoelEmaas <nemaas.dev@gmail.com>
2024-07-07 15:12:07 +0800

    feat: add AuthorsJsonDBService in providers

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
+import { AuthorsJsonDBService } from './authors-json-db.service';
+import { Module } from '@nestjs/common';
 
 @Module({
   controllers: [AuthorsController],
-  providers: [AuthorsService]
+  providers: [AuthorsService, AuthorsJsonDBService]
 })
+
 export class AuthorsModule {}
