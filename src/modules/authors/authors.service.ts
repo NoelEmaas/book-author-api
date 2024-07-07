42ad36d5d940ef8f8e5208ff16196059b3b9f71c
NoelEmaas <nemaas.dev@gmail.com>
2024-07-07 20:58:22 +0800

    feat: implemented  getAuthoredBooks method and added validation in delete author

--- src/modules/authors/authors.service.ts
+++ src/modules/authors/authors.service.ts
@@ -1,12 +1,17 @@
 import { AuthorsJsonDBService } from './authors-json-db.service';  
 import { AuthorsServiceBase } from './authors-abstract.service';
 import { AuthorType } from 'src/types/author.types';
+import { BooksJsonDBService } from '../books/books-json-db.service';
+import { BookType } from 'src/types/book.types';
 import { CreateAuthorDto, UpdateAuthorDto } from './dto';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AuthorsService implements AuthorsServiceBase {
-  constructor(private readonly authorsJsonDBservice: AuthorsJsonDBService) {}
+  constructor(
+    private readonly authorsJsonDBservice: AuthorsJsonDBService,
+    private readonly booksJsonDBService: BooksJsonDBService
+  ) {}
 
   getAuthors(authorFilter: Partial<AuthorType> & { search?: string }): AuthorType[] {
     return this.authorsJsonDBservice.getAll(authorFilter);
@@ -20,6 +25,11 @@ export class AuthorsService implements AuthorsServiceBase {
     }
 
     return author;
+  }
+
+  getAuthoredBooks(id: string): BookType[] {
+    const authoredBooks = this.booksJsonDBService.getAll({ authorIds: [id] });
+    return authoredBooks;
   }
 
   createAuthor(createAuthorDto: CreateAuthorDto): AuthorType {
@@ -38,12 +48,18 @@ export class AuthorsService implements AuthorsServiceBase {
 
   deleteAuthor(id: string): AuthorType {
     const authorToRemove = this.authorsJsonDBservice.get(id);
+    const authoredBooks = this.getAuthoredBooks(id);
 
     if (!authorToRemove) {
       throw new NotFoundException('Author you are trying to delete does not exist');
     }
 
-    this.authorsJsonDBservice.get(id);
+    if (authoredBooks.length > 0) {
+      throw new BadRequestException('Author you are trying to delete has authored books');
+    }
+
+    this.authorsJsonDBservice.delete(id);
     return authorToRemove;
   }
+
 }
